516e721d3e8b9f39a1cfa43e431ce389e357cf04
Juan Manuel Hernandez Marcano <juanhm93@gmail.com>
2020-05-26 01:42:26 -0400

    convertido el componente course en una clase, ya pudimos adelantan los llamadas a los eventos de las funciones falta darle la ejecucion correcta

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -1,25 +1,59 @@
 import React from 'react';
-import CourseButton from './CourseButton';
 import './styles/Course.css';
 import './styles/Lines.css';
 // import automaquillajeExpress from '../assets/static/automaquillaje-express.jpg';
 // import automaquillajeEstilismo from '../assets/static/automaquillaje-y-estilismo.jpg';
 // import automaquillajeTexas from '../assets/static/automaquillaje-texas.jpg';
 
-const Course = ({children}) => (
-    <section id="cursos" className="course">
-    <div className="title-special">
-        <hr className="line-course l-left" /><h3>cursos</h3><hr className="line-course l-right" /> 
-    </div>
-    <div className="course__container">
-        <div className="course__container--details">
-            {children}
-        </div>
-        <div className="direction">
-          <CourseButton/>
-        </div>
-    </div>
-</section>
-);
+class Course extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            indice: 1,
+            style:[
+                {
+                  left: '-100'  
+                }
+            ]
+        };
+        this.nextSlide = this.nextSlide.bind(this);
+        this.prevSlide = this.prevSlide.bind(this);
+    }
+
+    nextSlide(){
+        console.log("next1 "+this.state.indice); 
+        this.setState((state) => ({
+            indice: this.state.indice + 1
+        }));
+        console.log("next2 "+this.state.indice); 
+    
+    }
+    prevSlide(){
+        this.setState((state) => ({
+            indice: this.state.indice - 1
+        }));
+        console.log("prev "+this.state.indice);      
+    }
+
+    render(){
+        return(
+            <section id="cursos" className="course">
+                <div className="title-special">
+                    <hr className="line-course l-left" /><h3>cursos</h3><hr className="line-course l-right" /> 
+                </div>
+                <div  className="course__container">
+                    <div style={this.state.style.left} className="course__container--details">
+                        {this.props.children}
+                    </div>
+                    <div className="direction">
+                        <button  id="prev" className="prev" onClick={this.prevSlide}>&#10094;</button>
+                        <button  id="next" className="next" onClick={this.nextSlide}>&#10095;</button>
+                    </div>
+                </div>
+            </section>    
+
+        );
+    }
+}
 
 export default Course;
--- src/components/CourseButton.jsx
+++ src/components/CourseButton.jsx
@@ -9,13 +9,12 @@ class CourseButton extends React.Component{
         this.nextSlide = this.nextSlide.bind(this);
     }
     nextSlide (){
-        
+
     }
     render(){
         return(
             <>
-                <button  id="prev" className="prev" onClick="nextSlide(-1)">&#10094;</button>
-                <button  id="next" className="next" onClick="nextSlide(1)">&#10095;</button>
+               
             </>
         );
     }
