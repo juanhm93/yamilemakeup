755513b7a862a5dc639d38fd0b5f04c7c1ebbb50
Juan Manuel Hernandez Marcano <juanhm93@gmail.com>
2020-05-31 01:28:08 -0400

    cambios para ventana emergente realizado

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -4,7 +4,7 @@ import './styles/Popup.css';
 class Popup extends React.Component {  
   render() {  
 return (  
-<div className='popup' style={{  transform: this.props.show ? 'translateY(0vh)' : 'translateY(-100vh)'}} >  
+<div className='popup' >  
 <div className='popup-inner'>  
 <button onClick={this.props.closePopup}>Cerrar</button>  
 <h1>{this.props.text}</h1>  
